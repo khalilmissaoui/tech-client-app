3f33d56ec4b4adce39b40a0a8851f03b398f2ebc
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-01-27 15:11:26 +0100

    initilizing the application

---
+++ tech-client-appointment/src/test/java/com/practice/techclientappointment/TechClientAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.techclientappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechClientAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
