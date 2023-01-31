6e20159fe6804cb6d868b808e72fc436f216fd03
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-01-31 09:35:45 +0100

    add Appointment entity

---
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/entity/Appointment.java
@@ -0,0 +1,50 @@
+package com.practice.techclientappointment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table( name = "T_APPOINTMENT")
+
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long appointmentId;
+
+    private String price;
+
+    @Temporal (TemporalType.DATE)
+    private Date time ;
+
+
+    @ManyToOne(
+            optional = false
+
+    )
+    @JoinColumn(
+            name = "client_id",
+            referencedColumnName = "clientId"
+    )
+    private Client client;
+
+    @ManyToOne(
+
+            optional = false
+
+    )
+    @JoinColumn(
+            name = "tech_id",
+            referencedColumnName = "techId"
+    )
+    private Technician technician;
+
+}
