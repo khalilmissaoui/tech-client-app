7f93c07badc9d3bf73f9336add0487f9fc753d97
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-01-31 10:33:23 +0100

    add Slice/Pagination/sorting features in findAll() clients

--- tech-client-appointment/src/test/java/com/practice/techclientappointment/repository/ClientRepositoryTest.java
+++ tech-client-appointment/src/test/java/com/practice/techclientappointment/repository/ClientRepositoryTest.java
@@ -2,13 +2,22 @@ package com.practice.techclientappointment.repository;
 
 import com.practice.techclientappointment.entity.Address;
 import com.practice.techclientappointment.entity.Client;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Slf4j
 class ClientRepositoryTest {
 
     @Autowired
@@ -35,6 +44,45 @@ class ClientRepositoryTest {
 
             clientRepository.save(student);
         }
+
+
+    }
+
+    @Test
+    public void findClientsByType() {
+
+        String SEARCHED_CLIENT_TYPE="agency 12";
+
+        int listLength = 3 ;
+        Slice<Client> slice = null;
+        Pageable pageable = PageRequest.of(0, listLength, Sort.by("type"));
+
+        while (true) {
+            slice = clientRepository.findAll(pageable);
+            List<Client> clientsList = slice.getContent();
+
+
+            assertTrue(clientsList.size() == listLength );
+
+            Boolean SuscpectedCleint = clientsList.stream().anyMatch(client -> client.getType().equalsIgnoreCase(SEARCHED_CLIENT_TYPE));
+
+            if (SuscpectedCleint){
+
+                log.info("CLIENTS are collected");
+
+                break;
+            }
+
+            if (!slice.hasNext()) {
+                log.info("END OF LIST OF PAGES");
+                break;
+
+            }
+
+            pageable = slice.nextPageable();
+        }
+
+
 
 
     }
