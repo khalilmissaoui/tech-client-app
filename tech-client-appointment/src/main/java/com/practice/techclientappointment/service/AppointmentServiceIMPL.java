31686df8764076974643c025dbd3e01f1e013244
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-08 19:05:23 +0100

    add description comments ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/service/AppointmentServiceIMPL.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/service/AppointmentServiceIMPL.java
@@ -4,7 +4,6 @@ import com.practice.techclientappointment.entity.Appointment;
 import com.practice.techclientappointment.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -18,26 +17,29 @@ public class AppointmentServiceIMPL implements IAppointmentService  {
 
     public Appointment addAppointment(Appointment appointment) {
 
-
+        // add not null appointment to be saved and returned
             return appointmentRepository.save(appointment);
-
 
     }
 
-    //try catch au niveau du controlleur
     public Appointment findAppointmentById(Long id){
 
+        //use Long param id to find and return an appointment
         //Improvement -- use DTO
         return returnAppointmentIfExistById(id);
     }
 
     public List<Appointment> findAllAppointments() {
 
+        // return a list of appointments
+
         //Improvement -- use DTO
         return appointmentRepository.findAll();
     }
 
     public List<Appointment> findAppointmentByTechnicianId(Long id) {
+
+        // return a list of appointments from db filtered by technician id (Long)
 
         //Improvement -- use DTO
         //Improvement -- check if tech exist and find by tech instead of id
@@ -47,7 +49,8 @@ public class AppointmentServiceIMPL implements IAppointmentService  {
 
     public List<Appointment> findAppointmentByClientId(Long id) {
 
-        //Improvement -- check if client exist and find by client instead of id
+        // return a list of appointments from db filtered by client id (Long)
+
         return appointmentRepository.findByClientId(id);
     }
     //add validation of data
@@ -55,20 +58,23 @@ public class AppointmentServiceIMPL implements IAppointmentService  {
     // annotation de validation de startup validation
     public void deleteAppointmentById(Long id) {
 
+        //use Long param id to delete an appointment
 
-            returnAppointmentIfExistById(id);
+        returnAppointmentIfExistById(id);
             appointmentRepository.deleteById(id);
 
     }
-// un petit commentaire pour une idee general - regles metier specifique - condition - work flow
     public Appointment updateAppointment(Appointment appointment) {
 
+        // take not null appointment to update and return the updated value
         returnAppointmentIfExistById(appointment.getAppointmentId());
         return  appointmentRepository.save(appointment);
+
     }
 
 
     private Appointment returnAppointmentIfExistById (Long id) throws RuntimeException{
+        // take not null appointment to update and return the updated value
         // improve --> throw custom exception
         return appointmentRepository.findById(id).orElseThrow(
                 () -> new RuntimeException(" Error : Appointment you are trying to handle does not exist ")
