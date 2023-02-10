a03564eab8b806fafd1b72c6d04909fb0da86b04
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-10 11:56:08 +0100

    add validate as default interface to avoid code duplication and keep chance to scale the validate inteface  ;

---
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/validations/ValidateEntity.java
@@ -0,0 +1,22 @@
+package com.practice.techclientappointment.validations;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public interface ValidateEntity<T> {
+    
+    // Validate generic objects and throws and RTE in case of invalidity
+    default void validate(T objectToValidate) {
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
+
+        if (!violations.isEmpty()) {
+            throw new RuntimeException("---------------------+++++++++++ unvalidated object");
+        }
+    }
+}
