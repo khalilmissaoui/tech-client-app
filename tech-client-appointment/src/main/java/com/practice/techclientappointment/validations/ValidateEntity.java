91c5c7766b2c8ddb5487cf5c57b30a8d902b2fea
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-10 12:02:45 +0100

    add throws RTE to extend the exception scope  ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/validations/ValidateEntity.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/validations/ValidateEntity.java
@@ -7,9 +7,9 @@ import javax.validation.ValidatorFactory;
 import java.util.Set;
 
 public interface ValidateEntity<T> {
-    
+
     // Validate generic objects and throws and RTE in case of invalidity
-    default void validate(T objectToValidate) {
+    default void validate(T objectToValidate) throws RuntimeException {
 
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
