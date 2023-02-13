fc8710324286dda2d36d164e2c3594a72e07e609
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-13 15:13:39 +0100

    add message validation annotation ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/BaseException.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/BaseException.java
@@ -1,12 +1,11 @@
 package com.practice.techclientappointment.exceptions;
 
+import com.practice.techclientappointment.validations.annotations.ExceptionMessageValidation;
 import org.springframework.http.HttpStatus;
-
-import javax.validation.constraints.NotNull;
 
 public abstract class BaseException extends RuntimeException {
 
-    public BaseException(@NotNull String message) {
+    public BaseException(@ExceptionMessageValidation String message) {
         super(message);
     }
 
