0a52f0bf3d55a43b4500171c755f99ee9030f6c1
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-13 16:27:23 +0100

    add param constructor ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
@@ -1,10 +1,15 @@
 package com.practice.techclientappointment.exceptions;
 
+import com.practice.techclientappointment.validations.annotations.ExceptionMessageValidation;
 import org.springframework.http.HttpStatus;
 
 public class NotValidObjectException extends BaseException {
     public NotValidObjectException() {
         super("appointmentSERVICE - Not valid object");
+    }
+
+    public NotValidObjectException(@ExceptionMessageValidation String message) {
+        super(message);
     }
 
     @Override
