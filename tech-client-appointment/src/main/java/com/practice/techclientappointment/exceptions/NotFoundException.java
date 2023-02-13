28e6d285f064d67b3b305b44bc8ede1b48255ab7
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-13 15:14:40 +0100

    update class with implicit EXCEPTIONS messages  ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotFoundException.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends BaseException {
 
-    public NotFoundException(String message) {
-        super(message);
+    public NotFoundException() {
+        super("appointmentSERVICE - Not found element");
     }
 
     @Override
--- tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
@@ -3,8 +3,8 @@ package com.practice.techclientappointment.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class NotValidObjectException extends BaseException {
-    public NotValidObjectException(String message) {
-        super(message);
+    public NotValidObjectException() {
+        super("appointmentSERVICE - Not valid object");
     }
 
     @Override
