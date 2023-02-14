ef3b7b288c4d4c3c88b98c85642dcafd43b3e646
khalilmissaoui <khalil.missaoui1@esprit.tn>
2023-02-14 12:09:24 +0100

    add set<String> constructor  NotValidObjectException class to handle various violation  ;

--- tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
+++ tech-client-appointment/src/main/java/com/practice/techclientappointment/exceptions/NotValidObjectException.java
@@ -3,16 +3,27 @@ package com.practice.techclientappointment.exceptions;
 import com.practice.techclientappointment.exceptions.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
 
+import java.util.Set;
+
 public class NotValidObjectException extends BaseException {
+
     public NotValidObjectException() {
 
         super(new ErrorMessage("appointmentService", "Invalid object"));
 
     }
 
+    public NotValidObjectException(Set<String> errorsList) {
+
+        super(new ErrorMessage("appointmentService", errorsList.toString()));
+
+    }
+
+
     public NotValidObjectException(ErrorMessage message) {
         super(message);
     }
+    
 
     @Override
     public HttpStatus getStatusCode() {
